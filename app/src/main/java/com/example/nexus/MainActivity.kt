d828e77827960cbfed9301d2c988311cbe3c2b70
deyvid <rospamoreia16@gmail.com>
2025-09-04 03:27:06 -0300

    #Solução do BUG de tela piscar ao entrar com biometria

--- app/src/main/java/com/example/nexus/MainActivity.kt
+++ app/src/main/java/com/example/nexus/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.nexus
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nexus.ui.navigation.MainApp
 import com.example.nexus.ui.theme.NexusTheme
@@ -11,7 +10,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             NexusTheme{
                 MainApp()
